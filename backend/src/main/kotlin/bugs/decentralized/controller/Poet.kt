8d813389cee826e39113bb80b2e857b61ed4517a
Mircox <mircea.rautoiu@gmail.com>
2022-09-20 18:17:13 +0300

    update computeWaitTime function

--- backend/src/main/kotlin/bugs/decentralized/controller/Poet.kt
+++ backend/src/main/kotlin/bugs/decentralized/controller/Poet.kt
@@ -1,11 +1,9 @@
 package bugs.decentralized.controller
 
-import bugs.decentralized.model.AccountAddress
 import bugs.decentralized.model.Block
 import bugs.decentralized.model.Node
 import bugs.decentralized.model.Transaction
 import bugs.decentralized.utils.SHA
-import java.math.BigInteger
 import kotlin.random.Random
 
 object Poet {
@@ -14,9 +12,10 @@ object Poet {
     private const val MAX_TIME = 120_000L //ms -> 2 min
 
     fun computeWaitTime(lastBlock: Block, nodeAddress: String): Long {
-        val hash = SHA.sha256Hex(lastBlock.computeHash() + nodeAddress)
-        val seed = BigInteger(hash, 16)
-        val rand = Random(seed.toLong())
+        val hash = SHA.sha256Hex(lastBlock.hash + nodeAddress)
+        //convert only the first 15 hex digits to avoid overflow
+        val seed = hash.substring(0, 15).toLong(16)
+        val rand = Random(seed)
         return rand.nextLong(MIN_TIME, MAX_TIME)
     }
 
