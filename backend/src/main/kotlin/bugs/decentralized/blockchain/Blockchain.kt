a700877a50b67ed80e5396915adc4cc79e099092
Mircox <mircea.rautoiu@gmail.com>
2022-09-20 19:41:01 +0300

    update verify function

--- backend/src/main/kotlin/bugs/decentralized/blockchain/Blockchain.kt
+++ backend/src/main/kotlin/bugs/decentralized/blockchain/Blockchain.kt
@@ -1,5 +1,6 @@
 package bugs.decentralized.blockchain
 
+import bugs.decentralized.BlockchainApplication.Companion.GENESIS_BLOCK
 import bugs.decentralized.controller.NodesService
 import bugs.decentralized.controller.Poet
 import bugs.decentralized.controller.ValidatorController
@@ -12,7 +13,6 @@ import org.bson.Document
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveCallback
 import org.springframework.stereotype.Component
-import java.lang.Exception
 
 @Component
 class BlockListener @Autowired constructor() : AfterSaveCallback<Block> {
@@ -98,21 +98,26 @@ class Blockchain @Autowired constructor(
         }
     }
 
-    //TODO make verify() useful
-//    fun verify() {
-//        check(blocks.isNotEmpty()) { "Blockchain can't be empty" }
-//        check(blocks[0] == GENESIS_BLOCK) { "Invalid first block!" }
-//
-//        for (i in 1 until blocks.size) {
-//            val current = blocks[i]
-//
-//            check(current.blockNumber == i.toLong()) { "Invalid block number ${current.blockNumber} for block #${i}!" }
-//
-//            val previous = blocks[i - 1]
-//            check(current.parentHash == previous.hash) { "Invalid previous block hash for block #$i!" }
-//
-//            /**cannot verify [expectedTime] against [waitingTime] (-> not stored anywhere)*/
-//            //check(isPoetValid(previous, )) { "Invalid waiting time for block #$i!" }
-//        }
-//    }
+    fun verify() {
+        val blocks = blockRepository.findAll()
+
+        check(blocks.isNotEmpty()) { "Blockchain can't be empty" }
+        check(blocks[0] == GENESIS_BLOCK) { "Invalid first block!" }
+
+        for (i in 1 until blocks.size) {
+            val current = blocks[i]
+            val previous = blocks[i - 1]
+
+            check(current.blockNumber == i.toLong()) { "Invalid block number ${current.blockNumber} for block #$i!" }
+
+            check(current.parentHash == previous.hash) { "Invalid previous block hash for block #$i!" }
+
+            check(
+                current.timestamp > previous.timestamp + Poet.computeWaitTime(
+                    previous,
+                    previous.nodeAddress
+                )
+            ) { "Invalid waitTime for block #$i!" }
+        }
+    }
 }
